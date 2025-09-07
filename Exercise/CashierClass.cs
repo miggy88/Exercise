cbc958a84c1ae383730015f3fb3e0770de929252
miggy88 <cunananluismiguel11@gmail.com>
2025-09-07 09:59:34 +0800

    STEP 3 Done

---
+++ Exercise/CashierClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+  
+  
+
+    public class CashierClass
+    {
+        private int x;
+        public static string getNumberInQueue = "";
+        public static Queue<string> CashierQueue;
+
+        public CashierClass()
+        {
+            x = 10000;
+            CashierQueue = new Queue<string>();
+        }
+
+        public string CashierGeneratedNumber(string CashierNumber)
+        {
+            x++;
+            CashierNumber = CashierNumber + x.ToString();
+            return CashierNumber;
+        }
+    }
+}
