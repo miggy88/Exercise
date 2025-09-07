b72c77c41a5b34b595bc90a0b55c560f4dcbc05d
miggy88 <cunananluismiguel11@gmail.com>
2025-09-07 11:19:43 +0800

    Step 9 Done

--- Exercise/CashierWindowQueueForm.Designer.cs
+++ Exercise/CashierWindowQueueForm.Designer.cs
@@ -41,6 +41,7 @@
             this.btnRefresh.TabIndex = 0;
             this.btnRefresh.Text = "Refresh";
             this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
             // btnNext
             // 
--- Exercise/CashierWindowQueueForm.cs
+++ Exercise/CashierWindowQueueForm.cs
@@ -4,17 +4,22 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Exercise
 {
-    public partial class CashierWindowQueueForm: Form
+    public partial class CashierWindowQueueForm : Form
     {
+        public ListBox ListCashierQueue { get; private set; } // Changed type to ListBox
+
         public CashierWindowQueueForm()
         {
             InitializeComponent();
+            ListCashierQueue = new ListBox(); // Initialize ListBox
+            Controls.Add(ListCashierQueue); // Add ListBox to the form
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -26,5 +31,22 @@ namespace Exercise
         {
 
         }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+        }
+
+        public void DisplayCashierQueue(System.Collections.IEnumerable CashierList)
+        {
+            ListCashierQueue.Items.Clear();
+            foreach (object obj in CashierList)
+            {
+                ListCashierQueue.Items.Add(obj.ToString()); 
+            }
+        }
     }
-}
+       
+        }
+    
+
